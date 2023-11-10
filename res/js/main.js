c01fe52f7964935daca15cee97ad7c31b2af5dc8
dedek <jozef.tomko@student.spsmb.cz>
2023-11-10 09:26:56 +0100

    konecne

---
+++ res/js/main.js
@@ -0,0 +1,43 @@
+const point = document.getElementById("point");
+const startButton = document.getElementById("startButton");
+const showTime = document.getElementById("showTime");
+
+let gameInterval;
+let gameIntervalSpeed = 500;
+let gameStart;
+
+startButton.onclick = () => {
+    startButton.style.display = "none"
+    setPointOnClick(point);
+    setGameInterval(point);
+    gameStart = performance.now();
+}
+
+const moveElement = (element, x, y) => {
+    element.style.top = y + "px";
+    element.style.left = x + "px";
+}
+
+const setPointOnClick = (element) => {
+    element.onclick = () => {
+        let gameEnd = performance.now();
+        let time = gameEnd - gameStart;
+        showTime.innerText = `Time: ${time}ms`
+        gameStart = gameEnd;
+        element.innerText++;
+        if (gameIntervalSpeed > 200){
+        gameIntervalSpeed -= 10;
+        setGameInterval(element);
+    }
+        moveElement(element, getRandomNumber(0, window.innerWidth - 85), getRandomNumber(0, window.innerHeight - 85));
+    };
+};
+
+const setGameInterval = (element) => {
+    clearInterval(gameInterval);
+    gameInterval = setInterval(() => {
+        moveElement(element, getRandomNumber(0, window.innerWidth - 85), getRandomNumber(0, window.innerHeight - 85));
+    }, gameIntervalSpeed)
+}
+
+const getRandomNumber = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
